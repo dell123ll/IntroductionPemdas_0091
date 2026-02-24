6521f8db82f2a84b009860511e01e5092faa3514
dell123ll <lia719650@gmail.com>
2026-02-24 18:59:23 +0700

    Create coba3.cpp

---
+++ coba3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int angka = 10;
+    float tinggi = 180.5;
+    double harga = 200000.00;
+    string nama = "aldi";
+
+    cout << "Selamat Datang" << endl;
+    cout << "di TI UMY\n";
+    cout << "Yogyakarta\n";
+
+    nama = "Abra";
+    cout << "Nama Saya : " << nama;
+    
+}
