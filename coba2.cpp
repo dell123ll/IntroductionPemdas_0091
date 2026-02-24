f25e9a34fbb15f077afbf7eadca2810764834668
dell123ll <lia719650.com>
2026-02-24 17:58:18 +0700

    coba2.cpp

---
+++ coba2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{ //start
+    float p,l,luas;
+//langkah 2 : Masukan Panjang
+cout << "masukan panjang : " ;
+cin >> p;
+//langka 3 : Masukan Lebar
+cout << "Masukan Lebar : " ;
+cin >> l;
+//langkah 4 : Hitung Luas = p x l
+luas = p*l;
+//langkah 5
+cout << "Hasilnya = " << luas ; 
+}//selesai
