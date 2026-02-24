e85344b1edc9fb268c1880bd6e1271a683c01a89
dell123ll <lia719650@gmail.com>
2026-02-24 17:13:19 +0700

    pesan

---
+++ coba.cpp
@@ -0,0 +1,19 @@
+//deklarasi library
+#include<iostream>
+using namespace std;
+
+//program utama 
+int main ()
+{ 
+//deklarasi variable umur
+int umur; 
+//memberi nilai dari variable 
+umur = 20; 
+
+//menampilkan isi string 
+cout << "Abra" << endl;
+//menampilkan isi integer 
+cout << "78" << endl;
+//menampilkan isi dari variable cout
+cout << umur << endl;
+}
